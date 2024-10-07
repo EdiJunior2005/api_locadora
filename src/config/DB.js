f1cb0f908da440b033d2acfe68c51dc6b5c0a38d
EdiJunior2005 <edicfjunior05@gmail.com>
2024-10-07 17:15:37 -0300

    ALTERAÇOES

--- src/config/DB.js
+++ src/config/DB.js
@@ -3,8 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async() => {
     try {
         await mongoose.connect(
-            `mongodb://localhost:27017/LOCADORA`
-          );
+            `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`
+          //`mongodb+srv://edicfjunior05:<edijunior05>@cluster0.54lgn.mongodb.net/LOCADORA`
+            );
           console.log(`conectado ao mongol`)
     } catch (error) {
         console.error('Erro ao conectar ao MongoDB', error);
